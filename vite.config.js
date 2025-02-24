99adf8bfaa063e09a3bf35d50738d08b575737d7
Yagocastelao <yago_castelau@hotmail.com>
2025-02-24 03:57:11 +0100

    Creada Demo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig({server: {
+  watch: {
+    usePolling: true,
+  },
+},
   plugins: [react(), tailwindcss()],
 })
-
