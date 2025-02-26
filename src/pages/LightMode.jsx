752aa58889bb0df24f0daca73e936d7b25215683
Yago Cima Castelao <yago_castelau@hotmail.com>
2025-02-26 10:57:22 +0100

    primeros cambios

--- src/pages/LightMode.jsx
+++ src/pages/LightMode.jsx
@@ -22,16 +22,16 @@ export function LightMode() {
           className="w-full h-full"
         >
           <div className="w-full h-full flex">
-            <div className="w-1/2 h-full flex flex-col justify-center items-center gap-10">
-              <div className="w-[570px] flex flex-col justify-center items-center bg-[#2D2D2D] rounded-lg overflow-hidden shadow-lg gap-2">
-                <div className="w-[410px] h-[350px] mt-3 shadow-lg">
+            <div className="w-1/2 h-full flex flex-col justify-center items-center gap-6">
+              <div className="w-[670px] flex flex-col justify-center items-center bg-[#2D2D2D] rounded-lg overflow-hidden shadow-lg gap-2">
+                <div className="w-[350px] h-[350px] mt-3 shadow-lg">
                   <img
                     src="/teste.jpg"
                     alt="Foto de exemplo"
                     className="w-full h-full object-cover"
                   />
                 </div>
-                <div className="bg-[#00CED1] p-4 h-[170px] m-3 rounded-2xl">
+                <div className="bg-[#00CED1] p-4 h-[200px] m-3 rounded-2xl">
                   <p className="text-md text-gray-500">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
                     do eiusmod tempor incididunt ut labore et dolore magna
@@ -63,19 +63,19 @@ export function LightMode() {
                 <Navbar />
               </div>
 
-              <div className="w-3/4 h-3/4 bg-[#2D2D2D] rounded-lg flex items-center justify-center">
-                <div className="w-[480px]">
-                  <div className="grid grid-cols-2 gap-8">
-                    <div className="w-[200px] h-[210px] bg-[#6B7280] rounded-lg flex items-center justify-center shadow-lg border-1 border-b-[#6B7280]">
+              <div className="w-[1000px] h-[700px] bg-[#2D2D2D] rounded-lg flex items-center justify-center mr-30">
+                <div className="w-[800px]">
+                  <div className="grid grid-cols-2 gap-8 p-10">
+                    <div className="w-[300px] h-[290px] bg-[#6B7280] rounded-lg flex items-center justify-center shadow-lg border-1 border-b-[#6B7280]">
                       <span className="text-[#E0E0E0]">Projecto I</span>
                     </div>
-                    <div className="w-[200px] h-[210px] bg-[#6B7280] rounded-lg flex items-center justify-center shadow-lg border-1 border-b-[#6B7280]">
+                    <div className="w-[300px] h-[290px] bg-[#6B7280] rounded-lg flex items-center justify-center shadow-lg border-1 border-b-[#6B7280]">
                       <span className="text-[#E0E0E0]">Projecto II</span>
                     </div>
-                    <div className="w-[200px] h-[210px] bg-[#6B7280] rounded-lg flex items-center justify-center shadow-lg border-1 border-b-[#6B7280]">
+                    <div className="w-[300px] h-[290px] bg-[#6B7280] rounded-lg flex items-center justify-center shadow-lg border-1 border-b-[#6B7280]">
                       <span className="text-[#E0E0E0]">Projecto III</span>
                     </div>
-                    <div className="w-[200px] h-[210px] bg-[#6B7280] rounded-lg flex items-center justify-center shadow-lg border-1 border-b-[#6B7280]">
+                    <div className="w-[300px] h-[290px] bg-[#6B7280] rounded-lg flex items-center justify-center shadow-lg border-1 border-b-[#6B7280]">
                       <span className="text-[#E0E0E0]">Projecto IV</span>
                     </div>
                   </div>
